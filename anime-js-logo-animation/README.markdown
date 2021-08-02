b6115c734b38c82916bf3b19f1ea3fdaded51ec5
rayct <rayct@icloud.com>
2021-08-02 13:58:03 +0100

    Removed anime-js folder and Increased font weight

--- anime-js-logo-animation/README.markdown
+++
@@ -1,5 +0,0 @@
-# anime.js logo animation
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/juliangarnier/pen/xOgyjB](https://codepen.io/juliangarnier/pen/xOgyjB).
-
-Logo animation for anime-js.com
--- text-effect/text-effect-app.js
+++ text-effect/text-effect-app.js
@@ -1,8 +1,3 @@
-// const logo = document.querySelectorAll("#logo path");
-
-// for (let i = 0; i < logo.length; i++) {
-//     console.log('Letter ${i} is ${logo[i].getTotalLength()}');
-// }
 
 document.addEventListener('DOMContentLoaded', () => {
   function animateSgv(id, delay, delayIncrement) {
