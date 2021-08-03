67067f3d3f3a3da22937abaa4767b6961a3c1d96
rayct <rayct@icloud.com>
2021-08-03 17:31:54 +0100

    Changed  the animation delay to 0.4s

--- src/ppw-scripts.js
+++ src/ppw-scripts.js
@@ -1,4 +1,4 @@
-//Get the button:
+// Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,4 +16,24 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
+
+// Text Effect Start
+document.addEventListener('DOMContentLoaded', () => {
+  function animateSgv(id, delay, delayIncrement) {
+    const logo = document.getElementById(id);
+    const logoPaths = document.querySelectorAll(`#${id} path`);
+    delay = delay;
+    for (let i = 0; i < logoPaths.length; i++) {
+      console.log(logoPaths[i].getTotalLength());
+      logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+      logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+      logoPaths[i].style.animation = `line-anim 1s ease forwards ${delay}s`;
+      delay += delayIncrement;
+      console.log(delay)
+    }
+    logo.style.animation = `fill 1s ease forwards ${delay}s`;
+  }
+  animateSgv('logo', 0, 0.4)
+}, false);
+// Text Effect End 
